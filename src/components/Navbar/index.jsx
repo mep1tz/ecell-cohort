f3d18fe3c71692166a5a6704c4b907231b84f5be
Navin P <mep1tz2002@gmail.com>
2024-02-28 14:12:00 +0530

    Changed padding to 30 for navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 function Navbar( {ecellLogo}) {
   return (
-    <div className="h-20 flex flex-row px-40 items-center w-full text-white bg-black border-b-2">
+    <div className="h-20 flex flex-row px-30 items-center w-full text-white bg-black border-b-2">
         <div className="h-auto justify-left items-left py-4 rounded-md">
           <img  src={ecellLogo} alt="image description" height={40} width={40}></img>
         </div>
